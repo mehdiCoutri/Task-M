cfa9b9fc0259ff1fdd146b581fcabe7d7157cc65
mehdiCoutri <132202965+mehdiCoutri@users.noreply.github.com>
2025-09-11 11:56:04 +0100

    Application.java

--- src/main/java/com/company/tasks/Application.java
+++ src/main/java/com/company/tasks/Application.java
@@ -6,14 +6,14 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyApplication extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(MyApplication.class);
+        return builder.sources(Application.class);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
