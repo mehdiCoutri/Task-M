050dc0a096c9382a7ff308d41336a57145dc1159
mehdiCoutri <132202965+mehdiCoutri@users.noreply.github.com>
2025-09-11 11:47:19 +0100

    Application.java

--- src/main/java/com/company/tasks/Application.java
+++ src/main/java/com/company/tasks/Application.java
@@ -2,16 +2,17 @@ package com.company.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-/**
- * This class start the spring boot application
- */
 @SpringBootApplication
 public class MyApplication extends SpringBootServletInitializer {
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(MyApplication.class);
     }
+
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
     }
